44a3a8cd8d89159bc8176f29c6651f955cf1438e
m1ke <midall01.md@gmail.com>
2021-01-05 11:30:10 +0100

    Implement the menu

--- battleship/src/de/htw/battleship/BattleshipApplication.java
+++ battleship/src/de/htw/battleship/BattleshipApplication.java
@@ -28,14 +28,77 @@ public class BattleshipApplication {
 
         // TODO print main menu to the console. let user select an option. (s. Aufgabe 3)
 
+        int option;
+        while (true) {
+            printMenu();
+            String input = new Scanner(System.in).nextLine();
+            try {
+                option = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                wrongInput();
+                continue;
+            }
+            if (option == 1) {
+                startNewGame();
+            } else if (option == 2) {
+                break;
+            } else {
+                wrongInput();
+                continue;
+            }
+        }
 
-        startNewGame();
+        while (hasRunningGame()) {
+            printMenu();
+            String input = new Scanner(System.in).nextLine();
+            try {
+                option = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                wrongInput();
+                continue;
+            }
+            switch (option) {
+                case 1:
+                    startNewGame();
+                    break;
+                case 2:
+                    continueGame();
+                    break;
+                case 3:
+                    loadGame();
+                    break;
+                case 4:
+                    saveGame();
+                    break;
+                case 5:
+                    this.game.running = false;
+                default:
+                    wrongInput();
+            }
+        }
 
-
-
-
-
+        System.out.println("Vielen Dank für's Spielen! Ich hoffe, Sie hatten Spaß. " +
+                "Bis zum nächsten Mal!");
     }
+
+    private static void wrongInput() {
+        System.out.println("Bitte wählen Sie eine der vorgegebenen Optionen " +
+                "(Zahl ohne Klammer) und bestätigen Sie mit ENTER.");
+    }
+
+    private void printMenu() {
+        String menuOutput = "(1) Neues Spiel starten\n";
+        if (hasRunningGame()) {
+            menuOutput += "(2) Spiel fortsetzen\n";
+            menuOutput += hasSavedGame() ? "(3) Spiel laden\n" : "";
+            menuOutput += "(4) Spiel speichern\n";
+            menuOutput += "(5) Beenden";
+        } else {
+            menuOutput += "(2) Beenden";
+        }
+        System.out.println(menuOutput);
+    }
+
 
     /**
      * Restores a game from the file "battleship.save"
